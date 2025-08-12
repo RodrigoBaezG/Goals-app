d5f43bc8d31cf906ba258026b4bdb565a3810b45
Rodrigo Baez <rkbaezgarcia@gmail.com>
2025-08-12 11:57:10 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="text-red-500">
+      <small>
+        &copy; 2025 Goals App. Developed and designed by Rodrigo Báez. All rights reserved.
+      </small>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div>
+      <div className="header">
+        <svg></svg>
+        <a>Goals app</a>
+      </div>
+      <nav className="flex"></nav>
+    </div>
+  );
+}
+
+export default Header;
