582d83a390a72f27f4b193f0ceba0b530cc9cc34
Rodrigo Baez <rkbaezgarcia@gmail.com>
2025-10-14 21:00:21 +0100

    github pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = "Goals-app";
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), react()],
+    base: `/${repoName}/`,
     server: {
         proxy: {
             // Si la URL de React empieza con '/api', redirige al backend
