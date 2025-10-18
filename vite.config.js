5ebf19004eb1ed9f851056ab893b6881905730b5
Rodrigo Baez <rkbaezgarcia@gmail.com>
2025-10-18 12:21:34 +0100

    base deleted and _redirects

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), react()],
-    base: './',
     server: {
         proxy: {
             // Si la URL de React empieza con '/api', redirige al backend
