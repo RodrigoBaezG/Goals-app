3c46cd22a286915909ca07fd238301deac541e59
Rodrigo Baez <rkbaezgarcia@gmail.com>
2025-10-20 13:02:34 +0100

    modal css

--- src/components/shared/Modal.jsx
+++ src/components/shared/Modal.jsx
@@ -1,7 +1,7 @@
 function Modal({ children }) {
     return (
         <div className="fixed inset-0 bg-gray-500/75 flex items-start justify-center pt-10 pb-20 px-4 z-40 overflow-y-auto">
-            <div className="w-full max-w-3xl max-h-full">{children}</div>
+            <div className="w-full translate-x-9 max-w-3xl max-h-full mb-4">{children}</div>
         </div>
     );
 }
